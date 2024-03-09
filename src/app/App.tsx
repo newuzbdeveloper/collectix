d0901b0d0e32e75ab21ac53580ed47c5b59c21e8
newuzbdeveloper <106350805+newuzbdeveloper@users.noreply.github.com>
2024-03-09 01:42:55 +0100

    feat/new dependencies ,theme,styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,16 @@
-function App() {
-  return <>Hello World!</>;
-}
+import AppRouter from "@config/routes/components/AppRouter";
+import theme from "@config/styles/theme";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import { SnackbarProvider } from "notistack";
 
-export default App;
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <SnackbarProvider>
+        <AppRouter />;
+      </SnackbarProvider>
+    </ThemeProvider>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@config': path.resolve(__dirname, './src/app/config/'),
+      '@store': path.resolve(__dirname, './src/app/store/'),
+      '@services': path.resolve(__dirname, './src/app/services/'),
+      '@hooks': path.resolve(__dirname, './src/app/hooks/'),
+    },
+  },
+});
