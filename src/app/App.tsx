48be42f1d155e826a3c5de1ca473b830fbbd45c0
newuzbdeveloper <106350805+newuzbdeveloper@users.noreply.github.com>
2024-03-12 00:09:53 +0100

    fix/fixing conflicts between branches

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,16 @@
+import AppRouter from "@config/routes/components/AppRouter";
+import theme from "@config/styles/theme";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import { SnackbarProvider } from "notistack";
+
 export default function App() {
-  return <>Hello World!</>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <SnackbarProvider>
+        <AppRouter />;
+      </SnackbarProvider>
+    </ThemeProvider>
+  );
 }
