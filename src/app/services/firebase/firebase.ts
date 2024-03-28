81588224fb8f6e7f5e0509a97681575664cdf8f6
newuzbdeveloper <106350805+newuzbdeveloper@users.noreply.github.com>
2024-03-28 03:01:28 +0100

    feat: firebase and home page update

--- src/app/services/firebase/firebase.ts
+++ src/app/services/firebase/firebase.ts
@@ -16,14 +16,3 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
 export const db = getFirestore(app);
-
-// try {
-//   const docRef = await addDoc(collection(db, 'users'), {
-//     first: 'Ada',
-//     last: 'Lovelace',
-//     born: 1815,
-//   });
-//   console.log('Document written with ID: ', docRef.id);
-// } catch (e) {
-//   console.error('Error adding document: ', e);
-// }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,12 @@
+import { AppRoutes } from '@config/routes/AppRoutes';
+import { Box, Typography, Link } from '@mui/material';
+
 export default function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <Box mt={5} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+      <Typography>Home Page of Collectix.</Typography>
+      <Link href={AppRoutes.signUp}>Sign Up</Link>
+      <Link href={AppRoutes.login}>Login</Link>
+    </Box>
+  );
 }
